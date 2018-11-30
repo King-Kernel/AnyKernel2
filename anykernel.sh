d7b8e6dd71fcbba0e5860a9224954540ee03d121
bdashore3 <bdashore3@gmail.com>
2018-11-30 16:03:43 -0500

    Add all anykernel files

---
+++ anykernel.sh
@@ -0,0 +1,75 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=KingKernel by kingbri@KingKernel and sweezie@KingKernel
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=marlin
+device.name2=sailfish
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+## AnyKernel install
+
+dump_boot;
+
+# begin ramdisk changes
+
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.lz4
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Found Magisk! Patching Kernel for boot tweaks and for preservation of magisk"; 
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D6673 77616E745F696E697472616D6673;
+    $bin/magiskboot --compress=lz4 $decompressed_image $compressed_image;
+    sleep 2;
+    ui_print " "; ui_print "Some of the Boot tweaks are pulled from Blackened Kernel Mod by xFirefly93"
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.lz4-dtb;
+fi;
+
+# Patch dtbo on custom ROMs
+hostname="$(file_getprop /system/build.prop "ro.build.host")"
+case "$hostname" in
+    *corp.google.com|abfarm*) host=google;;
+    *) host=custom;;
+esac
+if [ "$(file_getprop /system/build.prop "ro.build.user")" != "android-build" -o "$host" == "custom" ]; then
+  ui_print " "; ui_print "You are on a custom ROM, patching dtb to remove verity...";
+  $bin/magiskboot --dtb-patch /tmp/anykernel/Image.lz4-dtb;
+  #Remove custom kernel init.rcs
+  rm -rf /system/system/etc/init/init.benzoio.rc
+  ui_print " "; ui_print "Removed custom rom init.rcs"
+else
+  ui_print " "; ui_print "You are on stock, not patching dtb to remove verity!";
+fi;
+
+
+write_boot;
+
+## end install
+
